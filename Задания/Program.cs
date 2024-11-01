2afdb81fe744705ef0a85717e33e39756cdab842
slokv <slokv@DESKTOP-CUGUDRQ>
2024-11-01 12:20:21 +0300

    Добавьте файлы проекта.

---
+++ Задания/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+namespace Задания
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] fullName = { "Петров И.М.", "Сидоров А.А", "Иванова А.А." };
+            string[] jobTitle = { "Кладовщик", "Уборщик", "Секретарь" };
+            string newSurFullName, newJobTitle, surname; 
+            bool accountingOpen = true;
+            int value, numberDossier;
+            while (accountingOpen)
+            {
+                DrawMenu();
+                
+                value = Convert.ToInt32(Console.ReadLine());  // условие на ввод только числа
+                if (value <= 5 && value > 0)
+                {
+                    switch (value)
+                    {
+                        case 1:
+                            Console.Write("Введите ФИО: ");
+                            newSurFullName = Console.ReadLine();
+                            fullName = AddDossier(fullName, newSurFullName);
+                            Console.Write("Введите должность: ");
+                            newJobTitle = Console.ReadLine();
+                            jobTitle = AddDossier(jobTitle, newJobTitle);
+                            break;
+                        case 2:                            
+                            DossierOutput(fullName, jobTitle);
+                            break;
+                        case 3:
+                            Console.Write("Введите номер досье для удаления: ");
+                            numberDossier = Convert.ToInt32(Console.ReadLine());
+                            if (fullName.Length == 0)
+                            {
+                                Console.WriteLine("Невозможно удалить - Список пуст");
+                                break;
+                            }
+                            fullName = DeleteDossier(fullName, numberDossier);
+                            jobTitle = DeleteDossier(jobTitle, numberDossier);
+                            break;
+                        case 4:
+                            Console.Write("Введите фамилию для поиска: ");
+                            surname = Console.ReadLine();
+                            SearchSurname(fullName, jobTitle, surname);
+                            break;
+                        case 5:                            
+                            accountingOpen = false;
+                            break;
+                    }
+
+                    Console.WriteLine("Нажмите любую клавишу для продолжения");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                
+                    Console.WriteLine("Введена не верная комманда, попробуйте снова.\nНажмите любую клавишу для продолжения");
+                    Console.ReadKey();                    
+                    Console.Clear();
+                
+            }
+
+        }       
+
+        static void DrawMenu()
+        {
+            Console.Write($"Добро пожаловать в систему учета.\n" +
+                $"Вы можете совершить:\n" +
+                $"1) Добавить досье.\n" +
+                $"2) Вывести досье.\n" +
+                $"3) Удалить досье.\n" +
+                $"4) Поиск по фамилии.\n" +
+                $"5) Завершить работу\n" +
+                $"Выберите комманду: ");
+        }
+        static void DossierOutput(string[] fullName, string[] jobTitle)
+        {
+            if (fullName.Length == 0)
+            {
+                Console.WriteLine("Список пуст");
+            }
+            
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                Console.Write($"{i + 1}. {fullName[i]} - {jobTitle[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static string[] AddDossier(string[] arrayName, string name)
+        {
+            Array.Resize(ref arrayName, arrayName.Length + 1);
+            arrayName[arrayName.Length - 1] = name;
+            return arrayName;
+        }
+
+        static string[] DeleteDossier(string[] arrayName, int numberDossier)
+        {           
+           
+                arrayName[numberDossier - 1] = "-";
+                for (int i = 0; i < arrayName.Length - 1; i++)
+                {
+                    if (arrayName[i] == "-")
+                    {
+                        arrayName[i] = arrayName[++i];
+                        arrayName[i] = "-";
+                        --i;
+
+                    }
+                }
+
+            
+            
+            Array.Resize(ref arrayName, arrayName.Length - 1);
+            return arrayName;
+        }
+
+        static void SearchSurname(string[] arrayName, string[] arrayJob, string surname)
+        {
+            surname = surname.ToLower();
+            for (int i = 0; i < arrayName.Length; i++)
+            {
+                if (surname == arrayName[i].ToLower().Substring(0, arrayName[i].IndexOf(' ')))
+                {
+                    Console.WriteLine($"{i + 1}. {arrayName[i]} - {arrayJob[i]}");
+                }
+            }
+        }
+
+
+    }
+}
+
+/*
+Задача:
+Будет 2 массива: 1) фио 2) должность.
+
+Описать функцию заполнения массивов – досье, функцию форматированного вывода, функцию поиска по фамилии и функцию удаления досье.
+
+Функция расширяет уже имеющийся массив на 1 и дописывает туда новое значение.
+
+Программа должна быть с меню, которое содержит пункты:
+
+1) добавить досье.
+
+2) вывести все досье (в одну строку через “-” фио и должность с порядковым номером в начале)
+
+3) удалить досье
+
+4) поиск по фамилии
+
+
+
+ */
---
+++ Конвертер валют/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace training
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double balanceRub, balanceEu, balanceDol, countEu, countDol;
+            int dol = 100, eu = 120;
+            string course = $"Наш курс: DOL = {dol}, EU = {eu}";
+            Console.Write("Введите баланс рублей: ");
+            balanceRub = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баланс евро: ");
+            balanceEu = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите баланс долларов: ");
+            balanceDol = Convert.ToSingle(Console.ReadLine());
+            int variant;
+            while (true)
+            {
+                Console.WriteLine($"Ваш баланс RUB = {balanceRub}, EU = {balanceEu}, DOL = {balanceDol}");
+                Console.Write("Добрый день, рады видеть в нашем банке.\n" +
+                "Мы можем конвертировать следующую валюту:\n" +
+                course +
+                "\n1. rub => eu\n" +
+                "2. eu => rub\n" +
+                "3. rub => dol\n" +
+                "4. dol => rub\n" +
+                "если желаете выйти введите 5\n" +
+                "Выполнить операцию: ");
+                variant = Convert.ToInt32(Console.ReadLine());
+                if (variant == 5)
+                {
+                    break;
+                }
+                switch (variant)
+                {
+                    case 1:
+                        Console.Write("Введите кол-во евро: ");
+                        countEu = Convert.ToSingle(Console.ReadLine());
+                        if (balanceRub < eu * countEu)
+                        {
+                            Console.WriteLine("Недостаточно средств на балансе, попробуйте другую сумму.");
+                            break;
+                        }
+                        balanceRub -= eu * countEu;
+                        balanceEu += countEu;
+                        break;
+                    case 2:
+                        Console.Write("Введите кол-во евро: ");
+                        countEu = Convert.ToSingle(Console.ReadLine());
+                        if (balanceEu < countEu)
+                        {
+                            Console.WriteLine("Недостаточно средств на балансе, попробуйте другую сумму.");
+                            break;
+                        }
+                        balanceRub += eu * countEu;
+                        balanceEu -= countEu;
+                        break;
+                    case 3:
+                        Console.Write("Введите кол-во долларов: ");
+                        countDol = Convert.ToSingle(Console.ReadLine());
+                        if (balanceRub < dol * countDol)
+                        {
+                            Console.WriteLine("Недостаточно средств на балансе, попробуйте другую сумму.");
+                            break;
+                        }
+                        balanceRub -= countDol * dol;
+                        balanceDol += countDol;
+                        break;
+                    case 4:
+                        Console.Write("Введите кол-во долларов: ");
+                        countDol = Convert.ToSingle(Console.ReadLine());
+                        if (balanceDol < countDol)
+                        {
+                            Console.WriteLine("Недостаточно средств на балансе, попробуйте другую сумму.");
+                            break;
+                        }
+                        balanceRub += dol * countDol;
+                        balanceDol -= countDol;
+                        break;
+                    default:
+                        Console.WriteLine("неверная комманда, попробуйте снова");
+                        break;
+                }
+            }
+        }
+    }
+}
